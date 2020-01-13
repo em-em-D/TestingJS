a9e20edab10e068bf1c44da8197bf0f5ba6f67b6
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-01-13 10:25:37 +0000

    Adding the testing for reversing a string

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,4 +1,5 @@
 import capitalize from './capitalize'
 test('Capitalizing a string', () => {
     expect(capitalize("happy")).toBe("Happy");
+    expect(capitalize('hello, we are programmers')).toBe('Hello, we are programmers');
   });
