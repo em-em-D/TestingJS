62e9d7b3f0c29b2c3deeee10cedbce0e7305ed55
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-01-13 12:21:10 +0000

    Adding the ceapher testing

---
+++ ceasar_cipher.js
@@ -0,0 +1,28 @@
+export default function ceaserCipher(string, key){
+  const newLetters = [];
+  const newKey = key % 26;
+  for(const letter of string){
+    newLetters.push(getNewLetter(letter, newKey));
+
+  }
+  return newLetters.join('');
+}
+
+
+function getNewLetter(letter, key){
+
+
+let newLetterCode = letter.charCodeAt();
+if(newLetterCode >= 97 && newLetterCode <= 122){
+  newLetterCode += key
+return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96 + (newLetterCode % 122));
+
+} else if (newLetterCode >= 65 && newLetterCode <= 90){
+  newLetterCode += key;
+  return newLetterCode <= 90 ? String.fromCharCode(newLetterCode) : String.fromCharCode(64 + (newLetterCode % 90));
+}
+ else {
+  return letter;
+}
+
+}
